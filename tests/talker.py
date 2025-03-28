e8125a475f5dcc32f458aa2163c70ad96852163f
Michael Gross <mgstinger521@gmail.com>
2025-03-28 16:20:21 -0700

    Update .gitignore, remove unused files, and enhance requirements.txt with custom package links

--- tests/talker.py
+++ tests/talker.py
@@ -1,4 +1,10 @@
-from ..digital_twin import LocalPubSub
+import os
+import sys
+
+# Add the parent directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Import the LocalPubSub class from the digital_twin module
+from digital_twin import LocalPubSub
 
 talker = LocalPubSub(port=5000)
 
