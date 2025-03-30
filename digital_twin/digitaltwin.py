bfb7eff8c181abd57341f5fb76d864cdb6ae423c
Michael Gross <mgstinger521@gmail.com>
2025-03-30 02:48:55 -0700

    Refactor digital twin simulation setup to use dynamic UR5 model and mesh paths for improved clarity and maintainability

--- digital_twin/digitaltwin.py
+++ digital_twin/digitaltwin.py
@@ -2,13 +2,17 @@ import time
 import mujoco_toolbox as mjtb
 from mujoco_toolbox.controllers import real_time
 from agnostic_controller  import UR5
-from digital_twin import LocalPubSub
+from localpubsub import LocalPubSub
+from pathlib import Path
 
 UR5e = UR5()
 
+ur5_root = Path(__file__).parent / "sim/model/static/UniversalRobotics/UR5"
+ur5_model = str(ur5_root / "UR5.urdf")
+ur5_meshes = str(ur5_root / "meshes" / "collision")
 
 with (
-    mjtb.Wrapper("path/to/xml", controller=real_time) as ur5,
+    mjtb.Wrapper(ur5_model, meshdir=ur5_meshes, controller=real_time) as ur5,
     LocalPubSub(port=5_000) as sub
 ):
     ur5.liveView(show_menu=False)  # Open the simulation window
