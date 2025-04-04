6f03ac76c674db9e0227eb0fc050890b7d60266d
Michael Gross <mgstinger521@gmail.com>
2025-04-04 16:29:43 -0700

    Move ConfigurationSpace and Glovebox classes for robot constraints management

---
+++ digital_twin/constraints/__init__.py
@@ -0,0 +1,2 @@
+from .ConfigurationSpace import ConfigurationSpace
+from .Glovebox import Glovebox
--- digital_twin/env/__init__.py
+++
@@ -1,2 +0,0 @@
-from .constraints.ConfigurationSpace import ConfigurationSpace
-from .constraints.Glovebox import Glovebox
