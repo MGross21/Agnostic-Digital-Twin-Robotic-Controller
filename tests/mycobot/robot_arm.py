1cfcbc9eceb0a6bdaee0dd13b47551c700ef1259
Michael Gross <mgstinger521@gmail.com>
2025-06-15 01:27:02 -0700

    add: implement myCobot_pro600 model test and simulation setup

---
+++ tests/mycobot/robot_arm.py
@@ -0,0 +1,18 @@
+import mujoco_toolbox as mjtb
+from mujoco_toolbox import glovebox, WORLD_ASSETS
+from pathlib import Path
+from armctl import Pro600
+import numpy as np
+
+mjcf = str(Path(__file__).resolve().parent / "mycobot_pro600.xml")
+
+home_position = np.array(Pro600().HOME_POSITION)
+
+
+with mjtb.Simulation(mjcf) as sim:
+    sim._model.qpos0 = home_position
+    sim.gravity = np.array([0, 0, 0])
+    sim.launch(show_menu=False)
+
+    while True:
+        sim._data.qpos = home_position
