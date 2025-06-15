601e0c0fb447497a9fb00b89f36639f13674c574
Michael Gross <mgstinger521@gmail.com>
2025-06-14 18:32:35 -0700

    refactor: add comments for object Z heights and robot offset in robot control script

--- digital_twin/robot_control.py
+++ digital_twin/robot_control.py
@@ -1,4 +1,16 @@
 from armctl import *
+from communication.localpubsub import LocalPubSub
 
-with Pro600("192.168.1.159",5_001) as robot:
+### Object Z HEIGHTS ###
+# Table: + 150 mm
+# Inside Cup: + 165 mm
+
+### Robot Offset ###
+# X: - 165mm
+# Y: + 520.7 mm
+
+with (
+    Pro600("192.168.1.159", 5_001) as robot,
+    # LocalPubSub(port=5_002) as pubsub,
+):
     robot.home()
